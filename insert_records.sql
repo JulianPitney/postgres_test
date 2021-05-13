3f12b6d9e6668ca84ad680a86710d09f6f5cdb38
julian <julianpitney@gmail.com>
2021-05-13 13:18:44 -0400

    Casting dates

--- insert_records.sql
+++ insert_records.sql
@@ -8,9 +8,9 @@ INSERT INTO scientific_data.RESEARCHERS VALUES
 
 
 INSERT INTO scientific_data.ANIMALS VALUES
-    (1, null, null, {3, 3}, 1, 2021-05-13, null, 'Mountain Dew'),
-    (2, null, null, {3, 3}, 1, 2021-05-13, null, 'Orange Crush'),
-    (3, 1, 2, null, 1, 2022-05-13, null, 'Vanilla Coke');
+    (1, null, null, {3, 3}, 1, 2021-05-13::DATE, null, 'Mountain Dew'),
+    (2, null, null, {3, 3}, 1, 2021-05-13::DATE, null, 'Orange Crush'),
+    (3, 1, 2, null, 1, 2022-05-13::DATE, null, 'Vanilla Coke');
 
 
 INSERT INTO scientific_data.LIGHTSHEET_CONFIGS VALUES
@@ -23,6 +23,6 @@ INSERT INTO scientific_data.SAMPLE_PREP_PROTOCOLS VALUES
 INSERT INTO scientific_data.SAMPLES VALUES
     (1, 3, 1, 4, 2022-05-13, 2023-05-13, 'Brain Tissue', -60, 'Freezer B');
 
-INSERT INTO scientific_data.LIGHTSHEET_SCAN VALUES
+INSERT INTO scientific_data.LIGHTSHEET_SCANS VALUES
     (1, 1, 1, 2, 'TEST SCAN 1', '127.0.0.1:4003/C:\scans\TEST SCAN 1', 90000000, 500, 4000,
      4000, true, false, false, 2, 2, 300, null, null, 300);
